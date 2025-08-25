405f6858e36cae03ccd66fb17dfa602204a142bb
Bartek Dobrzanski <baarti02@gmail.com>
2025-08-26 00:36:43 +0200

    Created first version sidebar

--- src/pages/global/Sidebar.tsx
+++ src/pages/global/Sidebar.tsx
@@ -1,12 +1,145 @@
-// import {useState} from "react";
-// import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar"
+import React, {useState} from "react";
+import {Sidebar, Menu, MenuItem } from "react-pro-sidebar"
+// import 'react-pro-sidebar/dist/styles/StyledUl'
+import { Box, IconButton, Link, Typography, useTheme } from "@mui/material"
+import { tokens } from "../../themes.tsx";
+import link from "react-router-dom"
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
-const Sidebar = () => {
+
+type ItemProps = {
+    title: string;
+    to: string;
+    icon: React.ReactNode;
+    selected: string;
+    setSelected: (value: string) => void;
+}
+
+const Item = ({ title, to, icon, selected, setSelected }: ItemProps) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return (
-        <div>
-            Sidebar
-        </div>
+        <MenuItem
+            active={selected === title}
+            style={{ color: colors.grey[100] }}
+            onClick={() => setSelected(title)}
+            icon={icon}
+        >
+            <Typography>{title}</Typography>
+            <Link to={to}/>
+        </MenuItem>
+    )
+}
+
+const MySidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+    const [selected, setSelected] = useState('Dashboard');
+
+    return (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+                },
+            }}
+        >
+            <Sidebar collapsed={isCollapsed}>
+                <Menu>
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+                        style={{
+                            margin: "10px 0 20px 0",
+                            color: colors.grey[100]
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box
+                                display="flex"
+                                justifyContent="space-between"
+                                alignItems="center"
+                                ml="15px"
+                            >
+                                <Typography variant="h3" color={colors.grey[100]}>
+                                    ADMINS
+                                </Typography>
+                                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        )}
+                    </MenuItem>
+
+                    {!isCollapsed && (
+                        <Box mb="25px">
+                            <Box display="flex" justifyContent="center" alignItems="center">
+                                <img
+                                    alt="profile-user"
+                                    width="100px"
+                                    height="100px"
+                                    src={`../../assets/user.png`}
+                                    style={{ cursor: "pointer", borderRadius: "50%" }}
+                                />
+                            </Box>
+                            <Box textAlign="center">
+                                <Typography
+                                    variant="h2"
+                                    color={colors.grey[100]}
+                                    fontWeight="bold"
+                                    sx={{ m: "10px 0 0 0" }}
+                                >
+                                Bartek Dobrzański
+                                </Typography>
+                                <Typography
+                                    variant="h5"
+                                    color={colors.greenAccent[800]}
+                                >
+                                    Standard plan
+                                </Typography>
+                            </Box>
+                        </Box>
+                    )}
+
+                    <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+                        <Item
+                            title="Dashboard"
+                            to="/"
+                            icon={<HomeOutlinedIcon />}
+                            selected={selected}
+                            setSelected={setSelected}
+                         />
+
+
+                    </Box>
+                </Menu>
+            </Sidebar>
+        </Box>
     );
 }
 
-export default Sidebar;
+export default MySidebar;
