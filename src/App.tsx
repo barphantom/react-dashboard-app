df8c1ad9d3a6fcbe4dca3e94a0765e3fa4347291
Bartek <baarti02@gmail.com>
2025-09-10 23:20:16 +0200

    Added calendar

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ import Team from "./pages/team/index.tsx"
 import Invoices from "./pages/invoices/index.tsx"
 import Contacts from "./pages/contacts/index.tsx"
 import Form from "./pages/form/index.tsx"
+import Calendar from "./pages/calendar/index.tsx"
 // import Bar from "./pages/global/Bar.tsx"
 // import Line from "./pages/global/Line.tsx"
 // import Pie from "./pages/global/Pie.tsx"
 // import FAQ from "./pages/global/FAQ.tsx"
 // import Geography from "./pages/global/Geography.tsx"
-// import Calendar from "./pages/global/Calendar.tsx"
 
 
 function App() {
@@ -34,12 +34,12 @@ function App() {
                             <Route path="/contacts" element={<Contacts />} />
                             <Route path="/invoices" element={<Invoices />} />
                             <Route path="/form" element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
                             {/*<Route path="/bar" element={<Bar />} />*/}
                             {/*<Route path="/pie" element={<Pie />} />*/}
                             {/*<Route path="/line" element={<Line />} />*/}
                             {/*<Route path="/faq" element={<FAQ />} />*/}
                             {/*<Route path="/geography" element={<Geography />} />*/}
-                            {/*<Route path="/calendar" element={<Calendar />} />*/}
                         </Routes>
 
                     </main>
